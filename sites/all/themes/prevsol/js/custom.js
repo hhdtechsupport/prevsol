84f55a495d55a009f3765e1aefd6207511384fb2
Teo <teovitk@gmail.com>
2018-08-20 18:54:57 -0400

    fixed breadcrumbs

--- sites/all/themes/prevsol/js/custom.js
+++ sites/all/themes/prevsol/js/custom.js
@@ -1,20 +1,15 @@
 (function ($, Drupal) {
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
-      console.log("script works...");
       if ($('.read-bio-toggle', context).once('myBehavior').length > 0) {
-        console.log("button found");
         $('.read-bio-toggle').click(function (event) {
-          console.log('button clicked');
           if ($(this).hasClass("active")) {
             $(this).removeClass("active");
             $(this).parent().parent().parent().find('.bio-body').hide();
-            console.log('toggle up');
           }
           else {
             $(this).addClass("active");
             $(this).parent().parent().parent().find('.bio-body').show();
-            console.log('toggle down');
           }
         });
       }
