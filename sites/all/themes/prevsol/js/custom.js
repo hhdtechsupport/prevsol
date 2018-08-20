eaa8bd6f9671cb40b92c448badf7127ea4c53da1
Teo <teovitk@gmail.com>
2018-08-20 17:59:19 -0400

    fixed issue where script was loading several times while being logged in

--- sites/all/themes/prevsol/js/custom.js
+++ sites/all/themes/prevsol/js/custom.js
@@ -1,17 +1,20 @@
 (function ($, Drupal) {
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
-      if ($('.read-bio-toggle').length > 0) {
+      console.log("script works...");
+      if ($('.read-bio-toggle', context).once('myBehavior').length > 0) {
+        console.log("button found");
         $('.read-bio-toggle').click(function (event) {
-          event.preventDefault();
-          event.stopPropagation();
+          console.log('button clicked');
           if ($(this).hasClass("active")) {
             $(this).removeClass("active");
             $(this).parent().parent().parent().find('.bio-body').hide();
+            console.log('toggle up');
           }
           else {
             $(this).addClass("active");
             $(this).parent().parent().parent().find('.bio-body').show();
+            console.log('toggle down');
           }
         });
       }
