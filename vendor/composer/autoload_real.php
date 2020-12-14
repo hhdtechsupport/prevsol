f9fd12d0db0e01a5df335c880b8fbcc8e8109524
Teo Vitkauskas <tvitkauskas@edc.org>
2020-12-14 14:37:32 -0500

    installed missing jquery_ui libraries needed for better_exposed_filters module

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit9de9436f91c599173609d35a420479fb
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
